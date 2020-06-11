b044e3148b3b010c0140c08b69fd16ebe144e0bf
Jack Walton <jackrwal@gmaill.com>
2020-06-11 19:38:00 +0100

    Created base html template to be extended by other templates

--- myCSBlog/views.py
+++ myCSBlog/views.py
@@ -7,4 +7,4 @@ def postList(request):
 
     posts = posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  # query DB for post objects, ordered by published date
 
-    return render(request, 'CS_BSc_Yr1_bridging_coursework/post-list.html', {'posts' : posts }) # Render HTML for the Post List page, parsing posts query results
+    return render(request, 'myCSBlog/post-list.html', {'posts' : posts}) # Render HTML for the Post List page, parsing posts query results
